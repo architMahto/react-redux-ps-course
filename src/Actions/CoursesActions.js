02a3dace0a430bf705c80ca7a5638dfad8fd7541
Archit Mahto <architmahto@gmail.com>
2018-09-03 23:40:18 -0400

    Refactored getCourses thunk and CoursesList page

--- src/Actions/CoursesActions.js
+++ src/Actions/CoursesActions.js
@@ -5,12 +5,18 @@ export const createCourse = (course) => {
 	return {type: CoursesActionTypes.CREATE_COURSE, course};
 };
 
+export const getCoursesStarted = () => {
+	return {type: CoursesActionTypes.GET_COURSES_STARTED}
+};
+
 export const getCoursesSuccess = (courses) => {
 	return {type: CoursesActionTypes.GET_COURSES_SUCCESS, courses}
 };
 
 export const getCourses = () => {
 	return (dispatch) => {
+		dispatch(getCoursesStarted());
+
 		return MockCourseApi.getAllCourses()
 			.then(courses => {
 				dispatch(getCoursesSuccess(courses));
