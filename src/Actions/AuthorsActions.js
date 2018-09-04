7b46b1f5c6eb03458bad676b7755439a288ecb54
Archit Mahto <architmahto@gmail.com>
2018-09-03 23:42:25 -0400

    Refactored getAuthors thunk

--- src/Actions/AuthorsActions.js
+++ src/Actions/AuthorsActions.js
@@ -1,12 +1,18 @@
 import * as AuthorsActionTypes from '../ActionTypes/AuthorsActionTypes';
 import MockAuthorApi from '../Api/MockAuthorApi';
 
+export const getAuthorsStarted = () => {
+	return {type: AuthorsActionTypes.GET_AUTHORS_STARTED};
+};
+
 export const getAuthorsSuccess = (authors) => {
 	return {type: AuthorsActionTypes.GET_AUTHORS_SUCCESS, authors};
-}
+};
 
 export const getAuthors = () => {
 	return (dispatch) => {
+		dispatch(getAuthorsStarted());
+
 		return MockAuthorApi.getAllAuthors()
 			.then(authors => {
 				dispatch(getAuthorsSuccess(authors))
