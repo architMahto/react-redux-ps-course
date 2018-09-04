0760516078104dc9e17a440a09c87969506cf4f5
Archit Mahto <architmahto@gmail.com>
2018-09-03 22:14:52 -0400

    Added redux-thunk to project

--- src/Store/ConfigureStore.js
+++ src/Store/ConfigureStore.js
@@ -1,15 +1,16 @@
 import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import invariant from 'redux-immutable-state-invariant';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from '../Reducers/RootReducer';
 
+const middlewares = [thunk, invariant()];
+
 export default function configureStore(initialState) {
 	return createStore(
 		rootReducer,
 		initialState,
-		composeWithDevTools(
-			applyMiddleware(invariant())
-		)
+		composeWithDevTools(applyMiddleware(...middlewares))
 	)
 }
