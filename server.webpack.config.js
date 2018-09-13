d69ea1291a62ce2961f130866b722dbc19e7dec9
Archit Mahto <architmahto@gmail.com>
2018-09-13 05:55:16 -0400

    Adding server webpack config

---
+++ server.webpack.config.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+const paths = require('react-scripts/config/paths');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const publicPath = paths.servedPath;
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
+
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const nodeModules = {};
+fs.readdirSync(paths.appNodeModules)
+	.filter(x => {
+		return ['.bin'].indexOf(x) === -1;
+	})
+	.forEach(mod => {
+		nodeModules[mod] = `commonjs ${mod}`;
+	});
+
+module.exports = {
+	bail: true,
+	devtool: shouldUseSourceMap ? 'source-map' : false,
+	entry: [resolveApp('server/index.js')],
+	context: __dirname,
+	node: {
+		__filename: true,
+		__dirname: true
+	},
+	target: 'node',
+	output: {
+		filename: 'server.bundle.js',
+		path: paths.appBuild,
+		publicPath
+	},
+	module: {
+		strictExportPresence: true,
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['babel-preset-env']
+					}
+				}
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				use: 'json-loader'
+			}
+		]
+	},
+	externals: nodeModules
+};
